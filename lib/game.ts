7622f0aee6bb15d14e8947b8acb0d1b676ac4610
Maunder <maunderscott@gmail.com>
2019-08-26 19:05:20 +1200

    More formatting. Adding typescript hinting for pixi

--- lib/game.ts
+++ lib/game.ts
@@ -1,37 +1,36 @@
-import * as PIXI from 'pixi.js'
-import pixiLogoUrl from './pixijs-logo.url'
+import * as PIXI from "pixi.js";
+import pixiLogoUrl from "./pixijs-logo.url";
 
 export default class Game {
-  private _app : PIXI.Application
+    private _app: PIXI.Application;
 
-  constructor(app : PIXI.Application) {
-    this._app = app
-  }
+    constructor(app: PIXI.Application) {
+        this._app = app;
+    }
 
-  temp() : void {
+    temp(): void {
+        let logo = PIXI.Sprite.from(pixiLogoUrl);
 
-    let logo = PIXI.Sprite.from(pixiLogoUrl);
+        //const logo = P.Sprite.fromImage(pixiLogoUrl)
+        logo.anchor.set(0.5);
+        logo.x = this._app.screen.width / 2;
+        logo.y = this._app.screen.height / 2;
 
-    //const logo = P.Sprite.fromImage(pixiLogoUrl)
-    logo.anchor.set(0.5)
-    logo.x = this._app.screen.width / 2
-    logo.y = this._app.screen.height / 2
+        this._app.stage.addChild(logo);
+        this._app.ticker.add(delta => {
+            logo.rotation += 0.05 * delta;
+            logo.skew.x += 0.01 * delta;
+            logo.skew.y += 0.01 * delta;
+        });
+    }
 
-    this._app.stage.addChild(logo)
-    this._app.ticker.add(delta => {
-      logo.rotation += 0.05 * delta
-      logo.skew.x += 0.01 * delta
-      logo.skew.y += 0.01 * delta
-    })
-  }
+    start(): void {
+        console.log(PIXI);
 
-  start() : void {
-    console.log(PIXI);
+        // const loader = PIXI.Loader.shared;
 
-    // const loader = PIXI.Loader.shared;
+        // loader.add('person', '/assets/images/img_person_0.png').load(this.temp());
 
-    // loader.add('person', '/assets/images/img_person_0.png').load(this.temp());
-
-    this.temp()
-  }
+        this.temp();
+    }
 }

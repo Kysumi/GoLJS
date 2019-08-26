c84c3b413f9065356fac446c0d57770fa43d59dc
Maunder <maunderscott@gmail.com>
2019-08-26 19:09:22 +1200

    Prettier formatting

--- main.ts
+++ main.ts
@@ -4,31 +4,31 @@ import * as PIXI from "pixi.js";
 window.addEventListener("DOMContentLoaded", initGame);
 
 function removeExistingGame(): void {
-    const els = document.body.children;
-    if (els.length > 0) {
-        document.body.removeChild(els.item(0) as Node);
-    }
+  const els = document.body.children;
+  if (els.length > 0) {
+    document.body.removeChild(els.item(0) as Node);
+  }
 }
 
 function init(): PIXI.Application {
-    removeExistingGame();
-    const app = new PIXI.Application(window.outerWidth, window.outerHeight, {
-        backgroundColor: 0x1099bb
-    });
-    document.body.appendChild(app.view);
-    return app;
+  removeExistingGame();
+  const app = new PIXI.Application(window.outerWidth, window.outerHeight, {
+    backgroundColor: 0x1099bb
+  });
+  document.body.appendChild(app.view);
+  return app;
 }
 
 function initGame(): void {
-    const app = init();
-    const game = new Game(app);
-    game.start();
+  const app = init();
+  const game = new Game(app);
+  game.start();
 }
 
 // @ts-ignore
 if (module.hot) {
-    // @ts-ignore
-    module.hot.accept(function accept() {
-        initGame();
-    });
+  // @ts-ignore
+  module.hot.accept(function accept() {
+    initGame();
+  });
 }
